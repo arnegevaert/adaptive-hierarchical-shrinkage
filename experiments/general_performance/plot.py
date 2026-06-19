fb8d04b9595f7954c2a2ecdefe447c2716de96de
Arne Gevaert <arne.gevaert@ugent.be>
2026-06-19 15:51:52 +0200

    less args for plotting

--- experiments/general_performance/plot.py
+++ experiments/general_performance/plot.py
@@ -1,25 +1,27 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-import argparse
 import seaborn as sns
+import yaml
+from pathlib import Path
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("results_dir", type=str)
-    parser.add_argument("out_dir", type=str)
-    args = parser.parse_args()
+    with open("config/experiments.yaml", "r") as f:
+        config = yaml.safe_load(f)
 
-    for filename in os.listdir(args.results_dir):
+    results_dir = Path(config["output_dir"]) / "general_performance"
+    output_dir = Path(config["output_dir"]) / "general_performance" / "plot"
+
+    for filename in os.listdir(results_dir):
         if filename.endswith(".csv"):
             ds_name = filename.split(".")[0]
-            df = pd.read_csv(os.path.join(args.results_dir, filename))
+            df = pd.read_csv(os.path.join(results_dir, filename))
             score_key = df.columns[-1]
             plt.figure(figsize=(10, 6))
             sns.lineplot(data=df, x="lambda", y=score_key, hue="shrink_mode")
             plt.xscale("log")
             plt.savefig(
-                os.path.join(args.out_dir, f"{ds_name}.svg"),
+                os.path.join(out_dir, f"{ds_name}.svg"),
                 bbox_inches="tight",
             )
