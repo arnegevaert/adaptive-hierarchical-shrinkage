803631b5f61811e148606304c1dda93b46d67ace
Arne Gevaert <arne.gevaert@ugent.be>
2026-06-19 16:22:20 +0200

    Updated plot function for general_performance

--- experiments/general_performance/plot.py
+++ experiments/general_performance/plot.py
@@ -12,16 +12,22 @@ if __name__ == "__main__":
 
     results_dir = Path(config["output_dir"]) / "general_performance"
     output_dir = Path(config["output_dir"]) / "general_performance" / "plot"
+    os.makedirs(output_dir, exist_ok=True)
 
-    for filename in os.listdir(results_dir):
-        if filename.endswith(".csv"):
-            ds_name = filename.split(".")[0]
-            df = pd.read_csv(os.path.join(results_dir, filename))
-            score_key = df.columns[-1]
-            plt.figure(figsize=(10, 6))
-            sns.lineplot(data=df, x="lambda", y=score_key, hue="shrink_mode")
-            plt.xscale("log")
-            plt.savefig(
-                os.path.join(out_dir, f"{ds_name}.svg"),
-                bbox_inches="tight",
-            )
+    for subdir in ["classification_dt", "classification_rf", "regression"]:
+        os.makedirs(os.path.join(output_dir, subdir), exist_ok=True)
+
+        if os.path.isdir(os.path.join(results_dir, subdir)):
+            for filename in os.listdir(os.path.join(results_dir, subdir)):
+                print(f"Processing {subdir}/{filename}...")
+                if filename.endswith(".csv"):
+                    ds_name = filename.split(".")[0]
+                    df = pd.read_csv(os.path.join(results_dir, subdir, filename))
+                    score_key = df.columns[-1]
+                    plt.figure(figsize=(10, 6))
+                    sns.lineplot(data=df, x="lambda", y=score_key, hue="shrink_mode")
+                    plt.savefig(
+                        os.path.join(os.path.join(output_dir, subdir), f"{ds_name}.svg"),
+                        bbox_inches="tight",
+                    )
+                    plt.close()
