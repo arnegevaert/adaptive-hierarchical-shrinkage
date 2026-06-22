3aa2b18535d0a6962b7bc4b217d21d6df23edeed
Arne Gevaert <arne.gevaert@ugent.be>
2026-06-22 10:47:14 +0200

    Boxplot instead of lineplot for benchmark

--- experiments/general_performance/plot.py
+++ experiments/general_performance/plot.py
@@ -1,33 +1,45 @@
 import os
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import yaml
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import yaml
 
 if __name__ == "__main__":
     with open("config/experiments.yaml", "r") as f:
         config = yaml.safe_load(f)
 
     results_dir = Path(config["output_dir"]) / "general_performance"
-    output_dir = Path(config["output_dir"]) / "general_performance" / "plot"
+    output_dir = Path(config["output_dir"]) / "general_performance"
     os.makedirs(output_dir, exist_ok=True)
 
     for subdir in ["classification_dt", "classification_rf", "regression"]:
         os.makedirs(os.path.join(output_dir, subdir), exist_ok=True)
 
         if os.path.isdir(os.path.join(results_dir, subdir)):
+            dfs = []
             for filename in os.listdir(os.path.join(results_dir, subdir)):
                 print(f"Processing {subdir}/{filename}...")
                 if filename.endswith(".csv"):
                     ds_name = filename.split(".")[0]
                     df = pd.read_csv(os.path.join(results_dir, subdir, filename))
-                    score_key = df.columns[-1]
-                    plt.figure(figsize=(10, 6))
-                    sns.lineplot(data=df, x="lambda", y=score_key, hue="shrink_mode")
-                    plt.savefig(
-                        os.path.join(os.path.join(output_dir, subdir), f"{ds_name}.svg"),
-                        bbox_inches="tight",
-                    )
-                    plt.close()
+                    df["dataset"] = ds_name
+                    dfs.append(df)
+            total_df = pd.concat(dfs, ignore_index=True)
+            fig, ax = plt.subplots(figsize=(10, 6))
+            sns.boxplot(
+                data=total_df,
+                x="dataset",
+                y="ROC AUC" if "ROC AUC" in total_df.columns else "R2",
+                hue="shrink_mode",
+                ax=ax,
+                notch=True,
+                showcaps=False,
+                flierprops={"marker": "x"},
+            )
+
+            fig.savefig(
+                os.path.join(os.path.join(output_dir), f"{subdir}.svg"),
+                bbox_inches="tight",
+            )
